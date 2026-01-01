a462ecee2219463c891ab00a1897be50499ebf4b
Rasit Altunc <rasitaltunc@gmail.com>
2026-01-02 02:21:51 +0300

    fix: add x-admin-token to CORS headers for lead-timeline endpoint

--- api/admin/lead-timeline/[leadId].js
+++ api/admin/lead-timeline/[leadId].js
@@ -12,7 +12,7 @@ module.exports = async function handler(req, res) {
     // CORS
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-admin-token");
 
     if (req.method === "OPTIONS") return res.status(200).end();
 
