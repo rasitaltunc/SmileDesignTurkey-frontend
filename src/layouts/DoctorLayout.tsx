071a925b549d459d163b1fadcfa9cffcd0ebd710
Rasit Altunc <rasitaltunc@gmail.com>
2026-01-14 01:35:03 +0300

    fix: add Link to imports in DoctorLayout to prevent 'Can't find variable: Link' error

--- src/layouts/DoctorLayout.tsx
+++ src/layouts/DoctorLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, NavLink, Navigate, useLocation } from "react-router-dom";
+import { Outlet, NavLink, Navigate, useLocation, Link } from "react-router-dom";
 import { useAuthStore } from "@/store/authStore";
 
 export default function DoctorLayout() {
